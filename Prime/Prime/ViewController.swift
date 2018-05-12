efc253ac4379df535f22298960caffb63b767b73
Jackson, Ceri-anne (Associate Software Developer) <ceri-anne.jackson@sky.uk>
2018-05-12 13:50:42 +0100

    Initial commit

---
+++ Prime/Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime
+//
+//  Created by Jackson, Ceri-anne (Associate Software Developer) on 12/05/2018.
+//  Copyright © 2018 Jackson, Ceri-anne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
