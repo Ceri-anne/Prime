9f805a16f0ddc39eb9d46f7df0e05132575da951
Jackson, Ceri-anne (Associate Software Developer) <ceri-anne.jackson@sky.uk>
2018-05-12 14:32:49 +0100

    Set up answer checking and label

--- Prime/Prime/PrimeViewController.swift
+++ Prime/Prime/PrimeViewController.swift
@@ -10,23 +10,40 @@ import UIKit
 
 class PrimeViewController: UIViewController {
 
-    @IBOutlet weak var number: UILabel!
-    
-    @IBAction func buttonPressed(_ sender: Any) {
-        print("Button pressed")
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let answer = sender.titleLabel?.text
+        checkAnswer(answer!)
     }
     
     let primes = [2,3,5,7,11,13,17,19]
     let maximum = 20
+    var number: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        number.text = String(generateRandomInt(below: maximum))
+        number = generateRandomInt(below: maximum)
+        numberLabel.text = String(number)
     }
 
     func generateRandomInt(below maximum: Int) -> Int {
         return 3
     }
+    
+    func isPrime(_ number: Int) -> Bool {
+        return primes.contains(number)
+    }
+    
+    func checkAnswer(_ answer: String) {
+        let choiceIsPrime = answer == "YES"
+        let numberIsPrime = isPrime(number)
+        if choiceIsPrime == numberIsPrime {
+            resultLabel.text = "Correct"
+            resultLabel.textColor = .green
+        } else {
+            resultLabel.text = "Incorrect"
+            resultLabel.textColor = .red
+        }
+    }
 }
-
-
